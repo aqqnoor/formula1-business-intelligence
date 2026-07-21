ecd91cc6c22712ccbc0d3b25a9247b29467b6f50
aqqnoor <aknurorazbai7@gmail.com>
2026-07-21 19:26:44 +0500

    etl process including

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -2,15 +2,30 @@ import pandas as pd
 
 def convert_types(df):
     if "date" in df.columns:
-        df["date"] = pd.to_datetime(df["date"])
+        df["date"] = pd.to_datetime(df["date"], errors="coerce")
     if "dob" in df.columns:
-        df["dob"] = pd.to_datetime(df["dob"])
-    
-    if "number" in df.columns:
-        df["number"] = df["number"].astype("Int64")
+        df["dob"] = pd.to_datetime(df["dob"], errors="coerce")
+    if "fp1_date" in df.columns:
+        df["fp1_date"] = df["fp1_date"].to_datetime("datetime64[ns]", errors="coerce")
+
+    if "fp2_date" in df.columns:
+        df["fp2_date"] = df["fp2_date"].to_datetime("datetime64[ns]", errors="coerce")
+    if "fp3_date" in df.columns:
+        df["fp3_date"] = df["fp3_date"].to_datetime("datetime64[ns]", errors="coerce")
+    if "quali_date" in df.columns:
+        df["quali_date"] = df["quali_date"].to_datetime("datetime64[ns]", errors="coerce")
+    if "sprint_date" in df.columns:
+        df["sprint_date"] = df["sprint_date"].to_datetime("datetime64[ns]", errors="coerce")
 
     return df
     
+
+    if "number" in df.columns:
+        df["number"] = df["number"].to_datetime("Int64", errors="coerce")
+
+    if "points" in df.columns:
+        df["points"] = df["points"].to_datetime(float, errors="coerce")
+
 
 def handle_missing(df):
     df = df.dropna(subset=["date", "dob"])
